8de9c8ed8d5784802d4485e89c46624be0e76872
Trintin Affleck <t.affleck101@hotmail.com>
2025-01-28 13:10:49 -0700

    Next skeleton Files

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Box, Typography } from '@mui/material';
+
+export default function Home() {
+  return (
+    <Box>
+      <Typography>Next.js Skeleton with MUI</Typography>
+    </Box>
+  );
+}
